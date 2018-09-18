ace65758cd2ff1828e95251a9d0e94dca61a55e3
seanjib <seanjib@gmail.com>
2018-09-18 10:08:23 -0600

    Basic file structure for pip package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='CPTAC',
+	version='0.1',
+	description='Python packaging for CPTAC data',
+	url='http://github.com/PayneLab/CPTAC',
+	author='Dr. Samuel Payne',
+	author_email='sam_payne@byu.edu',
+	license='MIT',
+	packages=['CPTAC'],
+	zip_safe=False)
