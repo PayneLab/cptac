506456292e31c7d3eb4a5ca1bf4769dd6376655a
David <davidwadams12@gmail.com>
2018-09-19 09:21:09 -0600

    Format test

--- CPTAC/__init__.py
+++ CPTAC/__init__.py
@@ -2,5 +2,4 @@ def start():
 
     joke = u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... Beiherhund das Oder die Flipperwaldt gersput.'
 
-    welcome = "Welcome to our CPTAC data. Below are a list of main functions for this package: \nfunction1()\nfunction2()\nfunction3()"
-    return (welcome)
+    print("Welcome to our CPTAC data. Below are a list of main functions for this package: \nfunction1()\nfunction2()\nfunction3()")
