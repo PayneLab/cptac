686e53c73ddc6b757a1e045b0789e8e62ad267e2
David <davidwadams12@gmail.com>
2018-09-20 09:55:54 -0600

    various get functions for dataframes

--- CPTAC/__init__.py
+++ CPTAC/__init__.py
@@ -1,9 +1,31 @@
 import os
 from .dataframe import DataFrameLoader
 
+clinical = DataFrameLoader("CPTAC" + os.sep + "Data" + os.sep + "clinical.csv").createDataFrame()
+proteomics = DataFrameLoader("CPTAC" + os.sep + "Data" + os.sep + "proteomics.txt").createDataFrame()
+transcriptome = DataFrameLoader("CPTAC" + os.sep + "Data" + os.sep + "transcriptome.txt").createDataFrame()
+cna = DataFrameLoader("CPTAC" + os.sep + "Data" + os.sep + "CNA.txt").createDataFrame()
+phosphoproteomics = DataFrameLoader("CPTAC" + os.sep + "Data" + os.sep + "phosphoproteomics.txt").createDataFrame()
+
 def get_meta_completeness():
-    meta = DataFrameLoader("CPTAC" + os.sep + "Data" + os.sep + "clinical.csv").createDataFrame()
-    return meta
+    return clinical
+
+def get_meta_cols():
+    return clinical.columns
+
+def get_cohort_meta(cols):
+    return clinical[cols]
+
+def get_proteomics_quant(colon_ids):
+    return proteomics.loc(colon_ids)
+def get_proteomics_cols():
+    return proteomics.columns
+def get_transcriptome_cols():
+    return transcriptome.columns
+def get_cohort_proteomics(cols):
+    return proteomics[cols]
+def get_cohort_transcriptome(cols):
+    return transcriptome[cols]
 
 def start():
 
