aef09bbe76f8a149066b792c935347ccd17b57a1
David <davidwadams12@gmail.com>
2018-09-21 10:18:41 -0600

    Updated CPTAC.start()

--- CPTAC/__init__.py
+++ CPTAC/__init__.py
@@ -68,4 +68,4 @@ def start():
 
     joke = u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... Beiherhund das Oder die Flipperwaldt gersput.'
 
-    print("Welcome to our CPTAC data. Below are a list of main functions for this package: \nfunction1()\nfunction2()\nfunction3()")
+    print("Welcome to our CPTAC data. Enter CPTAC.help() to open our Github help page.")
