6b6370001e7520397290a65f278dbec378782c8c
seanjib <seanjib@gmail.com>
2019-01-14 14:17:15 -0700

    removed old build info

--- build/lib/CPTAC/queries.py
+++
@@ -1,7 +0,0 @@
-class Queries:
-    def __init__(self, dataframe):
-        self.dataframe = dataframe
-    def query(self, tumor_type, query_type, value):
-        pass
-        #based on query_type
-        #self.dataframe searched for value with type
