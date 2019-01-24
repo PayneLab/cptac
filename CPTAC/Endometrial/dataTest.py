2b321dac2b5a349d18cbc6d0059ad1d7eeeb7ee4
David <davidwadams12@gmail.com>
2019-01-24 13:04:09 -0700

    Update dataTest.py

--- CPTAC/Endometrial/dataTest.py
+++ CPTAC/Endometrial/dataTest.py
@@ -14,7 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
-import CPTAC
+import CPTAC.Endometrial as en
 
 class Basic:
     def __init__(self):
@@ -23,17 +23,17 @@ class Basic:
         print("Evaluating getters...")
         dataframes = []
         file_names = {}
-        dataframes.append(CPTAC.get_clinical()); file_names[len(dataframes)] = "clinical"
-        dataframes.append(CPTAC.get_proteomics()); file_names[len(dataframes)] = "proteomics"
-        dataframes.append(CPTAC.get_phosphoproteomics()); file_names[len(dataframes)] = "site phosphoproteomics"
-        dataframes.append(CPTAC.get_phosphoproteomics(gene_level = True)); file_names[len(dataframes)] = "gene phosphoproteomics"
-        dataframes.append(CPTAC.get_transcriptomics()); file_names[len(dataframes)] = "linear transcriptomics"
-        dataframes.append(CPTAC.get_transcriptomics(circular = True)); file_names[len(dataframes)] = "circular transcriptomics"
-        dataframes.append(CPTAC.get_transcriptomics(miRNA = True)); file_names[len(dataframes)] = "miRNA"
-        dataframes.append(CPTAC.get_CNA()); file_names[len(dataframes)] = "CNA"
-        dataframes.append(CPTAC.get_somatic()); file_names[len(dataframes)] = "parsed somatic maf"
-        dataframes.append(CPTAC.get_somatic(binary=True)); file_names[len(dataframes)] = "binary somatic"
-        dataframes.append(CPTAC.get_somatic(unparsed=True)); file_names[len(dataframes)] = "unparsed somatic maf"
+        dataframes.append(en.get_clinical()); file_names[len(dataframes)] = "clinical"
+        dataframes.append(en.get_proteomics()); file_names[len(dataframes)] = "proteomics"
+        dataframes.append(en.get_phosphoproteomics()); file_names[len(dataframes)] = "site phosphoproteomics"
+        dataframes.append(en.get_phosphoproteomics(gene_level = True)); file_names[len(dataframes)] = "gene phosphoproteomics"
+        dataframes.append(en.get_transcriptomics()); file_names[len(dataframes)] = "linear transcriptomics"
+        dataframes.append(en.get_transcriptomics(circular = True)); file_names[len(dataframes)] = "circular transcriptomics"
+        dataframes.append(en.get_transcriptomics(miRNA = True)); file_names[len(dataframes)] = "miRNA"
+        dataframes.append(en.get_CNA()); file_names[len(dataframes)] = "CNA"
+        dataframes.append(en.get_somatic()); file_names[len(dataframes)] = "parsed somatic maf"
+        dataframes.append(en.get_somatic(binary=True)); file_names[len(dataframes)] = "binary somatic"
+        dataframes.append(en.get_somatic(unparsed=True)); file_names[len(dataframes)] = "unparsed somatic maf"
         PASS = True
         for x in range(0,len(dataframes)):
             if dataframes[x] is None:
@@ -47,18 +47,18 @@ class Basic:
         print("Evaluating special getters...")
         results = []
         functions = {}
-        results.append(CPTAC.get_clinical_cols()); functions[len(results)] = "clinical_cols"
-        results.append(CPTAC.get_cohort_clinical(["Diabetes","BMI"])); functions[len(results)] = "cohort_meta"
-        results.append(CPTAC.get_proteomics_quant(["S018","S100"])); functions[len(results)] = "proteomics_quant"
-        results.append(CPTAC.get_proteomics_cols()); functions[len(results)] = "proteomics_cols"
-        results.append(CPTAC.get_transcriptomics_cols()); functions[len(results)] = "transcriptomics_cols"
-        results.append(CPTAC.get_cohort_proteomics(["A1BG","TP53"])); functions[len(results)] = "cohort_proteomics"
-        results.append(CPTAC.get_cohort_transcriptomics(["A1BG","TP53"])); functions[len(results)] = "cohort_transcriptomics"
-        results.append(CPTAC.get_cohort_cna(["SASS6","TTTY22"])); functions[len(results)] = "cohort_cna"
-        results.append(CPTAC.get_cohort_phosphoproteomics(["TP53-S315","AAAS-S541"])); functions[len(results)] = "cohort_phosphoproteomics"
-        results.append(CPTAC.get_patient_mutations("C3L-00157")); functions[len(results)] = "patient_mutations(Patient_Id)"
-        results.append(CPTAC.get_patient_mutations("S013")); functions[len(results)] = "patient_mutations(Clinical_Patient_Key)"
-        results.append(CPTAC.get_phosphosites("TP53")); functions[len(results)] = "phosphosites"
+        results.append(en.get_clinical_cols()); functions[len(results)] = "clinical_cols"
+        results.append(en.get_cohort_clinical(["Diabetes","BMI"])); functions[len(results)] = "cohort_meta"
+        results.append(en.get_proteomics_quant(["S018","S100"])); functions[len(results)] = "proteomics_quant"
+        results.append(en.get_proteomics_cols()); functions[len(results)] = "proteomics_cols"
+        results.append(en.get_transcriptomics_cols()); functions[len(results)] = "transcriptomics_cols"
+        results.append(en.get_cohort_proteomics(["A1BG","TP53"])); functions[len(results)] = "cohort_proteomics"
+        results.append(en.get_cohort_transcriptomics(["A1BG","TP53"])); functions[len(results)] = "cohort_transcriptomics"
+        results.append(en.get_cohort_cna(["SASS6","TTTY22"])); functions[len(results)] = "cohort_cna"
+        results.append(en.get_cohort_phosphoproteomics(["TP53-S315","AAAS-S541"])); functions[len(results)] = "cohort_phosphoproteomics"
+        results.append(en.get_patient_mutations("C3L-00157")); functions[len(results)] = "patient_mutations(Patient_Id)"
+        results.append(en.get_patient_mutations("S013")); functions[len(results)] = "patient_mutations(Clinical_Patient_Key)"
+        results.append(en.get_phosphosites("TP53")); functions[len(results)] = "phosphosites"
         PASS = True
         for x in range(0,len(results)):
             if results[x] is None:
@@ -72,18 +72,18 @@ class Basic:
         print("Evaluating utilities...")
         results = []
         functions = {}
-        results.append(CPTAC.compare_gene(CPTAC.get_proteomics(), CPTAC.get_transcriptomics(), "A1BG")); functions[len(results)] = "compare_gene"
-        results.append(CPTAC.compare_gene(CPTAC.get_proteomics(), CPTAC.get_transcriptomics(), ["A1BG","RPL11"])); functions[len(results)] = "compare_genes"
-        results.append(CPTAC.compare_clinical(CPTAC.get_proteomics(), "BMI")); functions[len(results)] = "compare_clinical"
-        results.append(CPTAC.compare_mutations(CPTAC.get_proteomics(),"TP53")); functions[len(results)] = "compare_mutations(Proteomics)"
-        results.append(CPTAC.compare_mutations(CPTAC.get_proteomics(),"TP53","AURKA")); functions[len(results)] = "compare_mutations(Proteomics with Somatic)"
-        results.append(CPTAC.compare_mutations(CPTAC.get_phosphoproteomics(), "IRS2")); functions[len(results)] = "compare_mutations(Phosphoproteomics)"
-        results.append(CPTAC.compare_mutations(CPTAC.get_phosphoproteomics(), "IRS2","PIK3CA")); functions[len(results)] = "compare_mutations(Phosphoproteomics with Somatic)"
-        results.append(CPTAC.compare_mutations_full(CPTAC.get_proteomics(),"TP53")); functions[len(results)] = "compare_mutations_full(Proteomics)"
-        results.append(CPTAC.compare_mutations_full(CPTAC.get_proteomics(),"TP53","AURKA")); functions[len(results)] = "compare_mutations_full(Proteomics with Somatic)"
-        results.append(CPTAC.compare_mutations_full(CPTAC.get_phosphoproteomics(), "IRS2")); functions[len(results)] = "compare_mutations_full(Phosphoproteomics)"
-        results.append(CPTAC.compare_mutations_full(CPTAC.get_phosphoproteomics(), "IRS2","PIK3CA")); functions[len(results)] = "compare_mutations_full(Phosphoproteomics with Somatic)"
-        results.append(CPTAC.compare_phosphosites("TP53")); functions[len(results)] = "compare_phosphosites"
+        results.append(en.compare_gene(en.get_proteomics(), en.get_transcriptomics(), "A1BG")); functions[len(results)] = "compare_gene"
+        results.append(en.compare_gene(en.get_proteomics(), en.get_transcriptomics(), ["A1BG","RPL11"])); functions[len(results)] = "compare_genes"
+        results.append(en.compare_clinical(en.get_proteomics(), "BMI")); functions[len(results)] = "compare_clinical"
+        results.append(en.compare_mutations(en.get_proteomics(),"TP53")); functions[len(results)] = "compare_mutations(Proteomics)"
+        results.append(en.compare_mutations(en.get_proteomics(),"TP53","AURKA")); functions[len(results)] = "compare_mutations(Proteomics with Somatic)"
+        results.append(en.compare_mutations(en.get_phosphoproteomics(), "IRS2")); functions[len(results)] = "compare_mutations(Phosphoproteomics)"
+        results.append(en.compare_mutations(en.get_phosphoproteomics(), "IRS2","PIK3CA")); functions[len(results)] = "compare_mutations(Phosphoproteomics with Somatic)"
+        results.append(en.compare_mutations_full(en.get_proteomics(),"TP53")); functions[len(results)] = "compare_mutations_full(Proteomics)"
+        results.append(en.compare_mutations_full(en.get_proteomics(),"TP53","AURKA")); functions[len(results)] = "compare_mutations_full(Proteomics with Somatic)"
+        results.append(en.compare_mutations_full(en.get_phosphoproteomics(), "IRS2")); functions[len(results)] = "compare_mutations_full(Phosphoproteomics)"
+        results.append(en.compare_mutations_full(en.get_phosphoproteomics(), "IRS2","PIK3CA")); functions[len(results)] = "compare_mutations_full(Phosphoproteomics with Somatic)"
+        results.append(en.compare_phosphosites("TP53")); functions[len(results)] = "compare_phosphosites"
         PASS = True
         for x in range(0,len(results)):
             if results[x] is None:
@@ -99,7 +99,7 @@ class Stats:
     def __init__(self):
         pass
     def evaluate(self, data, trait):
-        data_trait = CPTAC.compare_clinical(data, trait)
+        data_trait = en.compare_clinical(data, trait)
         threshold = .05 / len(data.columns)
         tscutoff = .5
         significantTests = []
@@ -139,13 +139,13 @@ Basic().evaluate_special_getters()
 Basic().evaluate_utilities()
 
 print("Plotting...")
-Plotter().plot(CPTAC.get_proteomics(), "A1BG","PTEN","scatterplot")
-Plotter().plot(CPTAC.get_clinical(), "Diabetes","BMI","barplot")
-Plotter().plot(CPTAC.get_clinical(), "Diabetes","BMI","boxplot")
+Plotter().plot(en.get_proteomics(), "A1BG","PTEN","scatterplot")
+Plotter().plot(en.get_clinical(), "Diabetes","BMI","barplot")
+Plotter().plot(en.get_clinical(), "Diabetes","BMI","boxplot")
 print("PASS")
 
 print("Running statistics...")
-message = Stats().evaluate(CPTAC.get_proteomics(), "Pathway_activity_p53")
+message = Stats().evaluate(en.get_proteomics(), "Pathway_activity_p53")
 print(message)
 
-print("Version:",CPTAC.version())
+print("Version:",en.version())
