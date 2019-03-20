27838168fb7e8060fa1ec4a54e36c4afaa226197
David <davidwadams12@gmail.com>
2019-03-20 14:09:04 -0600

    Removed Colon March 19th embargo warning.

--- CPTAC/Colon/__init__.py
+++ CPTAC/Colon/__init__.py
@@ -17,13 +17,6 @@ import pandas as pd
 from .dataframe import DataFrameLoader
 from .utilities import Utilities
 
-def warning():
-    print("\n","******PLEASE READ******")
-    warning = "WARNING: This data is under a publication embargo until March 19, 2019. CPTAC is a community resource project and data are made available rapidly after generation for community research use. The embargo allows exploring and utilizing the data, but the data may not be in a publication until March 19, 2019. Please see https://proteomics.cancer.gov/data-portal/about/data-use-agreement or enter embargo() to open the webpage for more details."
-    wrapped_list = textwrap.wrap(warning)
-    for line in wrapped_list:
-        print(line)
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
 data_directory = dir_path + os.sep + "Data" + os.sep
 path = data_directory + "*.*"
@@ -37,7 +30,6 @@ for file in files: #loops through files variable
     except IOError:
         print("Error reading", file)
         print("Check that all file names coincide with DataFrameLoader specs")
-warning()
 
 def get_clinical():
     return data.get("clinical")
