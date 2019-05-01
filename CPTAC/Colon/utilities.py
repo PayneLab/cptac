898dff831e241b072759d2cc177bf1c69d2f3ad6
Caleb Lindgren <calebmlindgren@gmail.com>
2019-04-30 19:26:00 -0600

    Update imports in utilities files

--- CPTAC/Colon/utilities.py
+++ CPTAC/Colon/utilities.py
@@ -11,6 +11,7 @@
 
 import pandas as pd
 import numpy as np
+import re
 
 class Utilities:
     def __init__(self):
--- CPTAC/Endometrial/utilities.py
+++ CPTAC/Endometrial/utilities.py
@@ -12,7 +12,6 @@
 import pandas as pd
 import numpy as np
 import re
-import math
 
 class Utilities:
 
