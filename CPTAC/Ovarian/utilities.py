0dc6fc4567022d0b6b86d04b65fa71089351c2af
Caleb Lindgren <calebmlindgren@gmail.com>
2019-05-02 11:43:44 -0600

    Update version, fix ovarian mutation merger

--- CPTAC/Ovarian/utilities.py
+++ CPTAC/Ovarian/utilities.py
@@ -214,8 +214,6 @@ class Utilities:
             return
 
         mutations = mutations.drop(columns=["Gene"]) # Drop the gene column due to every value being the same
-        mutations = mutations.set_index("Clinical_Patient_Key") # Set index as S*** number for merging
-        mutations = mutations.drop(columns=['Patient_Id']) # We don't need this column
         if not multiple_mutations: # Filter out multiple mutations for a single sample
             mutations = self.add_mutation_hierarchy(mutations) # Appends hierachy for sorting so correct duplicate can be kept
             mutations = mutations.sort_values(by = ["Clinical_Patient_Key","Mutation_Hierarchy"], ascending = [True,False]) # Sorts by patient key, then by hierarchy so the duplicates will come with the higher number first
