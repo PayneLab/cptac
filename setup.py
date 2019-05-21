4a50a59973e3bfac50300db135a4eed6f7cf1021
Caleb Lindgren <calebmlindgren@gmail.com>
2019-05-21 15:40:41 -0600

    Rename in a couple more places

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 version = {}
-with open('CPTAC/version.py') as fp:
+with open('cptac/version.py') as fp:
 	exec(fp.read(), version)
 
-setup(name='CPTAC',
+setup(name='cptac',
 	version=version['__version__'],
 	description='Python packaging for CPTAC data',
-	url='http://github.com/PayneLab/CPTAC',
+	url='http://github.com/PayneLab/cptac',
 	author='Dr. Samuel Payne',
 	author_email='sam_payne@byu.edu',
 	license='Apache 2.0',
-	packages=['CPTAC','CPTAC.Endometrial','CPTAC.Algorithms','CPTAC.Ovarian','CPTAC.Colon'],
+	packages=['cptac','cptac.endometrial','cptac.algorithms','cptac.ovarian','cptac.colon'],
 	install_requires=[
 		'numpy',
 		'pandas'
