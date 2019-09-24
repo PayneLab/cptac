deba4a01a067d17e54b4ddce44b47536c7b388a2
thomashmolina <thomas.h.molina@gmail.com>
2019-09-24 16:41:11 -0600

    finished up testing omics_to_omics joins and metadata_to_omics joins

--- tests/jointests.py
+++ tests/jointests.py
@@ -1,5 +1,7 @@
 import cptac
 import pytest
+import sys
+import traceback
 
 class JoinTest:
 	def __init__(self):
@@ -25,16 +27,47 @@ class JoinTest:
 
 	def testOmicsToOmics(self):
 		ds1 = self.datasets
-		ds2 = self.datasets
+		error = False
 		for dataset1 in ds1:
-			for dataset2 in ds2:
-				valid_omics = set(dataset1._valid_omics_dfs + dataset2._valid_omics_dfs)
-				for omic in valid_omics:
-					ds1.join_omics_to_omics=
+			valid_omics = set(dataset1._valid_omics_dfs).intersection(set(dataset1._data.keys()))
+			for omic in valid_omics:
+				for omic2 in valid_omics:
+					if omic == omic2:
+						continue
+					else:
+						print(f"\n\njoining {omic}, {omic2} in dataset {dataset1.get_cancer_type()}\n\n")
+						try:
+							cross = dataset1.join_omics_to_omics(df1_name=omic, df2_name=omic2)
+						except Exception as e:
+							print(f"Error: {e}\n\n")
+							print(f"In datasets: {dataset1.get_cancer_type()}, {omic} did not successfully join with {omic2}. \n\n")
+							traceback.print_exc()
+							sys.exit(0)
+		assert(not error)
+	
+	def testMetaDataToOmics(self):
+		ds1 = self.datasets
+		error = False
+		for dataset1 in ds1:
+			valid_omics = set(dataset1._valid_omics_dfs).intersection(set(dataset1._data.keys()))
+			valid_metadata = set(dataset1._valid_metadata_dfs).intersection(set(dataset1._data.keys()))
+			for omic in valid_omics:
+				for metadata in valid_metadata:
+					print(f"joining {omic}, {metadata} in dataset {dataset1.get_cancer_type()}\n\n")
+					try:
+						cross = dataset1.join_metadata_to_omics(metadata_df_name=metadata, omics_df_name=omic)
+					except Exception as e:
+						print(f"Error: {e}\n\n")
+						print(f"In datasets: {dataset1.get_cancer_type()}, {omic} did not successfully join with {metadata}. \n\n")
+						traceback.print_exc()
+						sys.exit(0)
+		assert(not error)
 
-				#if omic1 in dataset._data.keys() and omic2 in dataset.data.keys():
-					#do the join
+	def testLostData(self):
+		#test for lost data in the columns
+		return
 
 
 t = JoinTest()
-t.testOmicsToOmics()
+#t.testOmicsToOmics()
+t.testMetaDataToOmics()
