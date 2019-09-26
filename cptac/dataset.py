b408af3543393a16984001aa7f25a5b001321d21
Caleb Lindgren <calebmlindgren@gmail.com>
2019-09-26 12:13:44 -0600

    Explicitly set dataframe copy in DataSet.reduce_multiindex to deep=True

--- cptac/dataset.py
+++ cptac/dataset.py
@@ -234,7 +234,7 @@ class DataSet:
         pandas DataFrame: The dataframe, with the desired column index changes made.
         """
         # Make a copy, so the original dataframe is preserved
-        df = df.copy()
+        df = df.copy(deep=True)
 
         if levels_to_drop is not None:
             if df.columns.nlevels < 2:
