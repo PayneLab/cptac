8814c41a2fa95ebdf60899c60cf583e54a62cf66
cbminor <41760877+cbminor@users.noreply.github.com>
2020-03-13 12:58:46 -0600

    Updates get_interacting_proteins_wikipathways function

--- cptac/utils/__init__.py
+++ cptac/utils/__init__.py
@@ -588,21 +588,20 @@ def parse_hotspot(path, mut_df):
 This function takes a path to WikiPathways Dataframe file and protein name and returns a list of all the proteins that interact with it, using the pathways from the WikiPathways relsease file.
 This function loads the WikiPathways dataframe, and iterates through the row labelled with that protein name, return every protein in a pathway that also contains that protein.
 """
-
 def get_interacting_proteins_wikipathways(protein):
 
-	WikiPathwaysDataframePath = sys.argv[1] # What to put here? Should it be hardcoded?
-	proteinName = protein
+	WikiPathwaysDataframePath = "BioPlex_interactionList_v4a.tsv"
 
 	df = pd.read_csv(WikiPathwaysDataframePath, index_col=0)
 
-	if (proteinName in df.)
-	row = df.loc[proteinName]
-	filtered_df = df.loc[:, row.values.tolist()]
-	def has_true(values):
-		for val in values:
-			if val == True:
-				return True
-		return False
-	filtered_df_final = filtered_df.loc[filtered_df.apply(lambda row: has_true(row.values.tolist()), axis=1), :]
-	return filtered_df_final.index.tolist()
+	if (protein in df.columns):
+		row = df.loc[protein]
+		filtered_df = df.loc[:, row.values.tolist()]
+		def has_true(values):
+			for val in values:
+				if val == True:
+					return True
+			return False
+		filtered_df_final = filtered_df.loc[filtered_df.apply(lambda row: has_true(row.values.tolist()), axis=1), :]
+		return filtered_df_final.index.tolist()
+	return None # The protein was not found.
