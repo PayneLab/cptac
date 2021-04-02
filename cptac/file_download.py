d3eb62d65f5b6222c4362d6e6dc1a651a4335785
Caleb Lindgren <calebmlindgren@gmail.com>
2021-04-02 12:13:12 -0600

    Fix pickling error

--- cptac/file_download.py
+++ cptac/file_download.py
@@ -274,6 +274,27 @@ def download_file(url, path, server_hash, password=None, box_token=None, file_me
             print(" " * len(download_msg), end='\r') # Erase the downloading message
             return "wrong_password"
 
+# Set up a localhost server to receive access token
+app = Flask(__name__)
+
+@app.route('/receive')
+def receive():
+
+    # Get the temporary access code
+    code = request.args.get('code')
+
+    # Create our "lock flag" file
+    Path(LOCK_NAME).touch()
+
+    # Save the code
+    with open(CODE_FILE_NAME, "w") as code_file:
+        code_file.write(code)
+
+    # Remove lock flag
+    os.remove(LOCK_NAME)
+
+    return "Authentication successful. You can close this window."
+
 
 def get_box_token():
 
@@ -289,27 +310,6 @@ def get_box_token():
     client_id = "kztczhjoq3oes38yywuyfp4t9tu11it8"
     client_secret = "a5xNE1qj4Z4H3BSJEDVfzbxtmxID6iKY"
     login_url = f"{base_url}?client_id={client_id}&response_type=code"
-
-    # Set up a localhost server to receive access token
-    app = Flask(__name__)
-
-    @app.route('/receive')
-    def receive():
-
-        # Get the temporary access code
-        code = request.args.get('code')
-
-        # Create our "lock flag" file
-        Path(LOCK_NAME).touch()
-
-        # Save the code
-        with open(CODE_FILE_NAME, "w") as code_file:
-            code_file.write(code)
-
-        # Remove lock flag
-        os.remove(LOCK_NAME)
-
-        return "Authentication successful. You can close this window."
 
     # Start the server
     server = Process(target=app.run, kwargs={"port": "8003"})
