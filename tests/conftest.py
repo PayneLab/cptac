8888ab15092243c4332ca988f504ddc21fb3a63c
corbinday <raizingthebar@gmail.com>
2021-07-10 00:33:56 -0600

    add get_all_datasets method

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 import pytest
 import cptac
 
+# returns a pandas dataframe of datasets plus extra info
 @pytest.fixture(autouse=True, scope="session")
 def get_datasets_list():
     return cptac.list_datasets()
 
+# returns a list of dataset strings
 @pytest.fixture(scope="session")
 def get_public_datasets(get_datasets_list):
     data = get_datasets_list["Data reuse status"]
@@ -14,6 +16,7 @@ def get_public_datasets(get_datasets_list):
             public_datasets.append(i.lower())
     return public_datasets
 
+# returns list of dataset strings
 @pytest.fixture(scope="session")
 def get_restricted_datasets(get_datasets_list):
     data = get_datasets_list["Data reuse status"]
@@ -23,3 +26,7 @@ def get_restricted_datasets(get_datasets_list):
             restricted_datasets.append(i.lower())
     return restricted_datasets
 
+# return a list of dataset strings
+@pytest.fixture(scope="session")
+def get_all_datasets(get_public_datasets, get_restricted_datasets):
+    return get_public_datasets + get_restricted_datasets
