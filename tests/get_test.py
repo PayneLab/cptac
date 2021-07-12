e8635133089aeb38dc2302bc5881987147ae4e21
corbinday <raizingthebar@gmail.com>
2021-07-12 15:45:08 -0600

    add code for testing getters

--- tests/get_test.py
+++ tests/get_test.py
@@ -11,27 +11,79 @@
 
 import pytest
 import cptac
+from cptac.exceptions import DataFrameNotIncludedError
 
 class TestGet:
 
-    # return an iterable list of <cptac.[Dataset]> objects
+    '''
+    return an iterable dict of initialized of <cptac.[Cancer Type]> objects 
+        for the cancer types that have publicly available data
+    '''
     @pytest.fixture(scope="class")
-    def make_public_dataset_objects(self, get_public_datasets):
-        """figure out how to pull specific attributes out of cptac to call them"""
-       
-        for dataset in get_public_datasets:
+    def make_public_cancer_type_objects(self, get_public_datasets):
+        cancer_type_objects = {}
+
+        for cancer in get_public_datasets:
             try: 
-                method = getattr(cptac, dataset)
-                method()
+                method = getattr(cptac, cancer)
+                cancer_type_objects[cancer] = method()
             except:
-                pytest.fail(f"cptac.{dataset} object was unable to be created")
+                pytest.fail(f"cptac.{cancer} object was unable to be created")
+        
+        return cancer_type_objects
 
+    '''
+    returns dict of cancer types and their getters
+    format:
+    {
+        "cancer type name" : {
+            cancer type object : [list of getters]
+        },
+    }
+    '''
+    @pytest.fixture(scope="class")
+    def get_cancers_and_dataset_getters(self, make_public_cancer_type_objects):
+        cancer_types_and_getters = {}
 
-    def get_dataset_getters(self, dataset):
-        data_list = []
-        for attribute in getattr(dataset):
-            pass
+        # start with dict of "name" : obj pairs
+        cancer_objects = make_public_cancer_type_objects
+        # loop through cancer objects
+        for cancer in cancer_objects:
+            single_cancer_and_getter = {} # will have <cptac.Cancer> : [cptac.Cancer.get_dataset] pairs
+            name = cancer # "str"
+            cancer_obj = cancer_objects[name] # <cptac.Cancer>
 
+            # make list for dataset_obj's getters
+            getters = []
+            # loop through datasets in the current cancer type
+            for dataset in cancer_obj.get_data_list():
+                function_name = "get_" + dataset
+                if function_name.__contains__("CNV"):
+                    function_name = "get_CNV"
+                try: 
+                    g = getattr(cancer_obj, function_name)
+                    getters.append(g)
+                except:
+                    pytest.fail(f"Unable to produce {dataset} getter for the {cancer_obj} cancer type.")
+            
+            single_cancer_and_getter[cancer_obj] = getters
+            cancer_types_and_getters[cancer] = single_cancer_and_getter
 
-    def test_all_getters(self, make_public_dataset_objects):
-        pass
+        return cancer_types_and_getters
+
+    def test_all_getters(self, get_cancers_and_dataset_getters):
+        cancers_and_getters = get_cancers_and_dataset_getters # rename for readability
+
+        for cancer in cancers_and_getters.values():
+            for item in cancer.items():
+                cancer_obj = item[0]
+                getters = item[1]
+                for g in getters:
+                    try:
+                        if g == cancer_obj.get_CNV:
+                            g("log2ratio")
+                            g("gistic")
+                        else:
+                            g()
+                    except DataFrameNotIncludedError:
+                        pytest.fail(DataFrameNotIncludedError)
