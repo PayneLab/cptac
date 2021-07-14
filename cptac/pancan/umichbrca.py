99e2cfca15bd78d7739a96c82f623df24926154f
blhmc1 <42850027+blhmc1@users.noreply.github.com>
2021-07-14 11:14:08 -0600

    Update umichbrca.py
    apply results of linear regression - drop likely normal samples and average replicates

--- cptac/pancan/umichbrca.py
+++ cptac/pancan/umichbrca.py
@@ -37,8 +37,8 @@ class UmichBrca(Dataset):
 
         data_files = {
             "1.0": ["Report_abundance_groupby=protein_protNorm=MD_gu=2.tsv",
-                    "Report_abundance_groupby=multi-site_protNorm=MD_gu=2.tsv"
-         
+                    "Report_abundance_groupby=multi-site_protNorm=MD_gu=2.tsv",
+                    "prosp-brca-all-samples.txt"
             ]
         }
 
@@ -70,7 +70,8 @@ class UmichBrca(Dataset):
                 df.index.name = 'Patient_ID'
                 
                 # drop ending of CPT retrospective samples to match cptac
-                df = df.rename(index={'CPT0008140004':'CPT000814', 'CPT0018460005': 'CPT001846', '604':'CPT000814'}) # 604 mapped to CPT000814 in pdc index
+                df = df.rename(index={'CPT0008140004':'CPT000814', 'CPT0018460005': 'CPT001846', 
+                                      '604':'CPT000814'}) # 604 mapped to CPT000814 in pdc index
     
                 drop_cols = ['RetroIR', 'RetroIR.1',
                    'RefInt_Pool01', 'RefInt_Pool02', 'RefInt_Pool03', 'RefInt_Pool04',
@@ -80,8 +81,6 @@ class UmichBrca(Dataset):
                    'RefInt_Pool17']
                 # Drop quality control and ref intensity cols
                 df = df.drop(drop_cols, axis = 'index')
-                # Since cptac brca has no normal samples, the duplicates are treated as replicates
-                df = average_replicates(df)
                 self._data["proteomics"] = df
                 
             #Phosphoproteomics    
@@ -91,95 +90,90 @@ class UmichBrca(Dataset):
                 df[['Database_ID','Transcript_ID',"Gene_ID","Havana_gene","Havana_transcript","Transcript","Name","Site"]] = df.Index.str.split("\\|",expand=True)
                 df[['num1','start',"end","detected_phos","localized_phos","Site"]] = df.Site.str.split("_",expand=True) 
 
-                 # Some rows have at least one localized phosphorylation site, but also have other phosphorylations that aren't localized. We'll drop those rows, if their localized sites are duplicated in another row, to avoid creating duplicates, because we only preserve information about the localized sites in a given row. However, if the localized sites aren't duplicated in another row, we'll keep the row.
+                # Some rows have at least one localized phosphorylation site, but also have other 
+                # phosphorylations that aren't localized. We'll drop those rows, if their localized 
+                # sites are duplicated in another row, to avoid creating duplicates, because we only 
+                # preserve information about the localized sites in a given row. However, if the localized 
+                # sites aren't duplicated in another row, we'll keep the row.
                 unlocalized_to_drop = df.index[~df["detected_phos"].eq(df["localized_phos"]) & df.duplicated(["Name", "Site", "Peptide", "Database_ID"], keep=False)]# dectected_phos of the split "Index" column is number of phosphorylations detected, and localized_phos is number of phosphorylations localized, so if the two values aren't equal, the row has at least one unlocalized site
                 df = df.drop(index=unlocalized_to_drop)
 
                 df = df[df['Site'].notna()] # only keep columns with phospho site 
-                df = df.set_index(['Name', 'Site', 'Peptide', 'Database_ID']) # This will create a multiindex from these columns, in this order.
+                df = df.set_index(['Name', 'Site', 'Peptide', 'Database_ID']) # Create a multiindex in this order.
                 #drop columns not needed in df 
-                df.drop([ 'Gene', "Index","num1","start","end","detected_phos","localized_phos","Havana_gene","Havana_transcript","MaxPepProb","Gene_ID","Transcript_ID","Transcript"], axis=1, inplace=True)
-            
+                df.drop(['Gene', "Index", "num1", "start", "end", "detected_phos", "localized_phos", "Havana_gene", 
+                         "Havana_transcript", "MaxPepProb", "Gene_ID", "Transcript_ID", "Transcript"], axis=1, inplace=True)
                 df = df.T #transpose df 
                 ref_intensities = df.loc["ReferenceIntensity"]# Get reference intensities to use to calculate ratios 
-                df = df.subtract(ref_intensities, axis="columns") # Subtract reference intensities from all the values, to get ratios
+                df = df.subtract(ref_intensities, axis="columns") # Subtract ref intensities from all the values, to get ratios
                 df = df.iloc[1:,:] # drop ReferenceIntensity row 
-                
                 # drop ending of CPT retrospective samples to match cptac
-                df = df.rename(index={'CPT0008140004':'CPT000814', 'CPT0018460005': 'CPT001846', '604':'CPT000814'}) # 604 mapped to CPT000814 in pdc index
+                df = df.rename(index={'CPT0008140004':'CPT000814', 'CPT0018460005': 'CPT001846', 
+                                      '604':'CPT000814'}) # 604 mapped to CPT000814 in pdc index
                 
                 drop_cols = ['RetroIR','RetroIR.1','RefInt_Pool01-1','RefInt_Pool02-1',
- 'RefInt_Pool03-1','RefInt_Pool04-1','RefInt_Pool05-1','RefInt_Pool06-1','RefInt_Pool07-1','RefInt_Pool08-1',
- 'RefInt_Pool09-1','RefInt_Pool10-1','RefInt_Pool11-1','RefInt_Pool12-1','RefInt_Pool13-1','RefInt_Pool14-1',
- 'RefInt_Pool15-1','RefInt_Pool16-1','RefInt_Pool17-1']
-
+                             'RefInt_Pool03-1','RefInt_Pool04-1','RefInt_Pool05-1','RefInt_Pool06-1',
+                             'RefInt_Pool07-1','RefInt_Pool08-1','RefInt_Pool09-1','RefInt_Pool10-1',
+                             'RefInt_Pool11-1','RefInt_Pool12-1','RefInt_Pool13-1','RefInt_Pool14-1',
+                             'RefInt_Pool15-1','RefInt_Pool16-1','RefInt_Pool17-1']
                 # Drop quality control and ref intensity cols
                 df = df.drop(drop_cols, axis = 'index')
-                # Since cptac brca has no normal samples, the duplicates are treated as replicates
-                df = average_replicates(df)
                 self._data["phosphoproteomics"] = df
+            
+            if file_name == "prosp-brca-all-samples.txt":
+                df = pd.read_csv(file_path, sep = "\t")
+                df = df[['Participant', 'id', 'Type']]
+                self._helper_tables["map_ids"] = df
 
         print(' ' * len(loading_msg), end='\r') # Erase the loading message
-        formatting_msg = "Formatting dataframes..."
+        formatting_msg = f"Formatting {self.get_cancer_type()} v{self.version()} dataframes..."
         print(formatting_msg, end='\r')
+        
+        # Get patient IDs with normal samples or replicates (from mapping file)
+        # prosp-brca-all-samples.txt shows which patient IDs have normal samples and which have replicates. This file
+        # can be found on Box under CPTAC/cptac/pancan/helper_files 
+        # 7 IDs with replicates: '11BR031', '11BR053', '11BR036', '11BR060', '14BR005', '11BR011', '21BR010'
+        # 18 IDs with normals: '11BR074', '11BR073', '20BR007', '21BR010', '11BR017', '05BR029', '18BR003', '11BR030',
+        #   '01BR027','11BR025', '11BR047', '11BR028', '11BR020', '20BR008', '11BR024', '11BR023', '11BR015', '11BR006'
+        map_df = self._helper_tables["map_ids"]
+        # Get IDs with replicates 
+        replicate_list = map_df.loc[map_df.id.str.contains('REP')].Participant.to_list() 
+        # Get IDs with normals 
+        norm_df = map_df.loc[map_df.Type == 'Adjacent_Normal'] # get all patient_IDs with normal samples
+        norm_df.index = norm_df.Participant.apply(lambda x: x[1:]+'.1') #remove initial 'X' and add '.1' (did not correlate well)
+        not_tumor = norm_df.index.to_list()
+        
+        # Drop samples that don't correlate well with the original cptac tumor values (likely normals)
+        # The proteomics and phosphoproteomics file contained duplicate patient IDs with unique values. 18 of these were IDs 
+        # with both tumor and normal samples (according to mapping file). To find which ID contained values nearest to the 
+        # original cptac tumor measurements, we ran a linear regression to compare flagship values to each of the 
+        # duplicate patient ID values. The first occurrance of every patient ID in the file with a normal sample showed 
+        # a high correlation with the flagship values (average around 0.9). The second occurrence showed a low correlation 
+        # with the flagship tumor values (average around  0.2). In Brca, normal samples were dropped in downstream analysis 
+        # because of quality control issues. Therefore, we dropped the second occurrence of the 18 patient IDs with a normal 
+        # sample because they did not correlate well with the flagship tumor values and are likely normal samples.
+        # There were 8 replicates shown in the mapping file (prosp-brca-all-samples.txt). A linear regression was used to check
+        # that these correlated well with their respective flagship cptac tumor values. Replicates were averaged (consistent with 
+        # the handling of other replicates in the pancan module).
+        # notes:  21BR010.1 had a correlation of 0.275 (so dropped), and 21BR010.2 had correlation of 0.848 (so averaged)
+        # A file showing all the correlations can be found at: 
+        # https://docs.google.com/spreadsheets/d/1_wVNzig3CH77eu2ouUM_qZHW3tB1tnA02An1TKVZsQQ/edit?usp=sharing
+        
+        # Proteomics
+        prot = self._data["proteomics"]
+        prot = prot.loc[ ~ prot.index.isin(not_tumor)] # drop rows that don't correlate well with respective cptac tumor 
+        prot = average_replicates(prot)# average 8 replicates (determined from mapping file and correlations) 
+        # checked that the remaining IDs with ".i" are replicates
+        self._data["proteomics"] = prot
 
+        # Phosphoproteomics
+        phos = self._data["phosphoproteomics"]
+        phos = phos.loc[ ~ phos.index.isin(not_tumor)] # drop rows that don't correlate well with respective cptac tumor 
+        phos = average_replicates(phos)# average 8 replicates (determined from mapping file and correlations) 
+        #checked that the remaining IDs with ".i" are replicates
+        self._data["phosphoproteomics"] = phos
         
         self._data = sort_all_rows_pancan(self._data)  # Sort IDs (tumor first then normal)
         
-        # Get a union of all dataframes' indices, with duplicates removed
-        ###FILL: If there are any tables whose index values you don't want
-        ### included in the master index, pass them to the optional 'exclude'
-        ### parameter of the unionize_indices function. This was useful, for
-        ### example, when some datasets' followup data files included samples
-        ### from cohorts that weren't in any data tables besides the followup
-        ### table, so we excluded the followup table from the master index since
-        ### there wasn't any point in creating empty representative rows for
-        ### those samples just because they existed in the followup table.
-#        master_index = unionize_indices(self._data) 
-
-        # Use the master index to reindex the clinical dataframe, so the clinical dataframe has a record of every sample in the dataset. Rows that didn't exist before (such as the rows for normal samples) are filled with NaN.
-#        new_clinical = self._data["clinical"]
-#        new_clinical = new_clinical.reindex(master_index)
-
-        # Add a column called Sample_Tumor_Normal to the clinical dataframe indicating whether each sample was a tumor or normal sample. Use a function from dataframe_tools to generate it.
-
-        ###FILL: Your dataset should have some way that it marks the Patient IDs
-        ### of normal samples. The example code below is for a dataset that
-        ### marks them by putting an 'N' at the beginning of each one. You will
-        ### need to write a lambda function that takes a given Patient_ID string
-        ### and returns a bool indicating whether it corresponds to a normal
-        ### sample. Pass that lambda function to the 'normal_test' parameter of
-        ### the  generate_sample_status_col function when you call it. See 
-        ### cptac/dataframe_tools.py for further function documentation.
-        ###START EXAMPLE CODE###################################################
-#        sample_status_col = generate_sample_status_col(new_clinical, normal_test=lambda sample: sample[0] == 'N')
-        ###END EXAMPLE CODE#####################################################
-
-#        new_clinical.insert(0, "Sample_Tumor_Normal", sample_status_col)
-
-        # Replace the clinical dataframe in the data dictionary with our new and improved version!
-#        self._data['clinical'] = new_clinical
-
-        # Edit the format of the Patient_IDs to have normal samples marked the same way as in other datasets. 
-        
-        ###FILL: You will need to pass the proper parameters to correctly
-        ### reformat the patient IDs in your dataset. The standard format is to
-        ### have the string '.N' appended to the end of the normal patient IDs,
-        ### e.g. the  normal patient ID corresponding to C3L-00378 would be
-        ### C3L-00378.N (this way we can easily match two samples from the same
-        ### patient). The example code below is for a dataset where all the
-        ### normal samples have  an "N" prepended to the patient IDs. The
-        ### reformat_normal_patient_ids function erases that and puts a ".N" at
-        ### the end. See cptac/dataframe_tools.py for further function
-        ### documentation.
-        ###START EXAMPLE CODE###################################################
-#        self._data = reformat_normal_patient_ids(self._data, existing_identifier="N", existing_identifier_location="start")
-        ###END EXAMPLE CODE#####################################################
-
-        # Call function from dataframe_tools.py to sort all tables first by sample status, and then by the index
-#        self._data = sort_all_rows(self._data)
-
-        # Call function from dataframe_tools.py to standardize the names of the index and column axes
-#        self._data = standardize_axes_names(self._data)
 
         print(" " * len(formatting_msg), end='\r') # Erase the formatting message
