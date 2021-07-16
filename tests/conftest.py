51b1d7330651b94f3a24bbe029aa419d24c0258d
corbinday <raizingthebar@gmail.com>
2021-07-15 20:56:36 -0600

    simplify fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,32 +1,24 @@
 import pytest
 import cptac
 
-# returns a pandas dataframe of datasets plus extra info
-@pytest.fixture(autouse=True, scope="session")
-def get_datasets_list():
-    return cptac.list_datasets()
-
-# returns a list of dataset strings
+# returns a dict of dataset lists
+# key is accessibility: public or private
 @pytest.fixture(scope="session")
-def get_public_datasets(get_datasets_list):
-    data = get_datasets_list["Data reuse status"]
+def get_datasets_list():
+    data = cptac.list_datasets()["Data reuse status"]
+
     public_datasets = []
+    restricted_datasets = []
     for i in data.index:
         if data[i] == "no restrictions":
             public_datasets.append(i)
-    return public_datasets
-
-# returns list of dataset strings
-@pytest.fixture(scope="session")
-def get_restricted_datasets(get_datasets_list):
-    data = get_datasets_list["Data reuse status"]
-    restricted_datasets = []
-    for i in data.index:
-        if data[i] == "password access only":
+        else:
             restricted_datasets.append(i)
-    return restricted_datasets
+    
+    dataset_lists = {}
+    dataset_lists["public"] = public_datasets
+    dataset_lists["restricted"] = restricted_datasets
+    
+    return dataset_lists
 
-# return a list of dataset strings
-@pytest.fixture(scope="session")
-def get_all_datasets(get_public_datasets, get_restricted_datasets):
-    return get_public_datasets + get_restricted_datasets
+    
