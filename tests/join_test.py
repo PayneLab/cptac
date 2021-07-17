9d48c505678a2b06774083693b7e2d7a6d72db14
corbinday <raizingthebar@gmail.com>
2021-07-16 18:13:29 -0600

    add test for omics to omics joining

--- tests/join_test.py
+++ tests/join_test.py
@@ -10,6 +10,7 @@
 #   limitations under the License.
 
 import pytest
+import itertools
 import cptac
 
 """
@@ -24,5 +25,39 @@ TODO: Things that should happen in a join test:
 # TODO: Check use cases for standard usage and then try to mess that up
 class TestJoin:
 
-   def join_omics_to_omics_test(self):
-       pass
+    @pytest.fixture(scope="class")
+    def all_joiners(self):
+        joiners = set()
+        for attribute in dir(cptac.dataset.Dataset):
+            if attribute.startswith("join"):
+                joiners.add(attribute)
+        return joiners
+
+    @pytest.fixture(scope="class")
+    def all_omics_combos(self, get_public_dataset_objects):
+        omics_lists_by_cancer = dict()
+        
+        # iterate through cancer objects
+        cancer_sets = get_public_dataset_objects[0]
+        for (cancer_type, cancer_object) in cancer_sets.items():
+            omics_list = list() # list of omics datasets for current cancer
+            # iterate through datasets of each cancer
+            for (dataset, dimensions) in cancer_object.get_data_list().items():
+                # check for 'omics' datasets and add to omics list for current cancer
+                if dataset.__contains__("omics"):
+                    omics_list.append((dataset, dimensions)) #including dimensions
+            # make list of omics pairings to be used in join tests
+            omics_combos = [(a, b) for a, b in itertools.combinations(omics_list, 2)]
+            omics_lists_by_cancer[cancer_object] = omics_combos
+
+        return omics_lists_by_cancer
+
+    def test_join_omics_to_omics(self, all_omics_combos):
+        for (cancer_object, omics_combos) in all_omics_combos.items():
+            for (omics_1, omics_2) in omics_combos:
+                dimensions = 1
+                dataset = 0
+                expected_columns = omics_1[dimensions]["columns"] + omics_2[dimensions]["columns"]
+                joiner = getattr(cancer_object, "join_omics_to_omics")
+                df = joiner(omics_1[dataset], omics_2[dataset])
+                assert df.shape[1] == expected_columns
