33dffc9cf50ac8cff6df3a8b8b48af1f53481b03
corbinday <raizingthebar@gmail.com>
2021-09-24 20:41:08 -0600

    add a working version of join_metadata_to_omics

--- tests/join_test.py
+++ tests/join_test.py
@@ -65,7 +65,21 @@ class TestJoin:
                 assert df.shape[1] == expected_columns
 
     def test_join_metadata_to_omics(self, get_cancer_test_units):
-        pass
+        # loop through cancers
+        for cancer in get_cancer_test_units:
+            # generate metadata combos per cancer
+            combos = self._combinations(cancer.metadata, cancer.omics)
+            # test each combo
+            for c in combos:
+                # ds is for dataset
+                ds1 = c[0]
+                ds1_df = cancer.get_dataset(ds1)
+                ds2 = c[1]
+                ds2_df = cancer.get_dataset(ds2)
+                expected_columns = ds1_df.shape[1] + ds2_df.shape[1]
+                df = cancer.cancer_object.join_metadata_to_omics(ds1, ds2)
+                # verify the join worked based on column counts
+                assert df.shape[1] == expected_columns
 
     def test_join_metadata_to_mutations(self, get_cancer_test_units):
         pass
@@ -73,6 +87,10 @@ class TestJoin:
     def test_multi_join(self, get_cancer_test_units):
         pass
 
-    def _combinations(self, list1):
-        combo_list = [ (a, b) for a, b in itertools.combinations(list1, 2) ]
+    def _combinations(self, list1, list2=None):
+        if not list2:
+            combo_list = [ (a, b) for a, b in itertools.combinations(list1, 2) ] 
+        else:
+            combo_list = [ (a, b) for a, b in itertools.product(list1, list2) ]
+        
         return combo_list
