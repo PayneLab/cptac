533bfb425745460b1e136291aaf8476c63aad702
corbinday <raizingthebar@gmail.com>
2021-09-24 20:21:00 -0600

    add a working version of join_metadata_to_metadata

--- tests/join_test.py
+++ tests/join_test.py
@@ -25,13 +25,13 @@ TODO: Things that should happen in a join test:
 # TODO: Check use cases for standard usage and then try to mess that up
 class TestJoin:
     '''
-    Verify that each join produces a dataframe with the expected number of rows and columns
+    Tests to verify that each join produces a working dataframe
     '''
    
     def test_join_omics_to_omics(self, get_cancer_test_units):
         # loop through cancers
         for cancer in get_cancer_test_units:
-            # generate omics combos per cancer and iterate
+            # generate omics combos per cancer
             omics_combos = self._combinations(cancer.omics)
             # test each combo
             for o in omics_combos:
@@ -42,13 +42,27 @@ class TestJoin:
                 expected_columns = omics1_df.shape[1] + omics2_df.shape[1]
                 df = cancer.cancer_object.join_omics_to_omics(omics1, omics2)
                 # verify the join worked based on column counts
-                assert df.shape[1] == expected_columns[1]
+                assert df.shape[1] == expected_columns
 
     def test_join_omics_to_mutations(self, get_cancer_test_units):
         pass
 
     def test_join_metadata_to_metadata(self, get_cancer_test_units):
-        
+        # loop through cancers
+        for cancer in get_cancer_test_units:
+            # generate metadata combos per cancer
+            combos = self._combinations(cancer.metadata)
+            # test each combo
+            for c in combos:
+                # ds is for dataset
+                ds1 = c[0]
+                ds1_df = cancer.get_dataset(ds1)
+                ds2 = c[1]
+                ds2_df = cancer.get_dataset(ds2)
+                expected_columns = ds1_df.shape[1] + ds2_df.shape[1]
+                df = cancer.cancer_object.join_metadata_to_metadata(ds1, ds2)
+                # verify the join worked based on column counts
+                assert df.shape[1] == expected_columns
 
     def test_join_metadata_to_omics(self, get_cancer_test_units):
         pass
