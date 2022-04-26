c9a5c52080b4350b29b820823019d9983f10d297
Corbin Day <raizingthebar@gmail.com>
2022-04-25 21:28:21 -0600

    rename for clarity

--- cptac/tools/download_tools/download_utils.py
+++ cptac/tools/download_tools/download_utils.py
@@ -259,3 +259,22 @@ def hash_bytes(bytes):
     hasher.update(bytes)
     hash = hasher.hexdigest()
     return hash
+
+def download_text(url):
+    """Download text from a direct download url for a text file.
+
+    Parameters:
+    url (str): The direct download url for the text.
+
+    Returns:
+    str: The downloaded text.
+    """
+    
+    try:
+        response = requests.get(url, headers=HEADERS, allow_redirects=True)
+        response.raise_for_status() # Raises a requests HTTPError if the response code was unsuccessful
+    except requests.RequestException: # Parent class for all exceptions in the requests module
+        raise NoInternetError("Insufficient internet. Check your internet connection.") from None 
+
+    text = response.text.strip()
+    return text
