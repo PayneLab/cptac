58a7426527bcc2c08b9a7cd76736462929b6dce7
Robert Oldroyd <oldroydrobert@gmail.com>
2022-06-02 14:57:33 -0600

    minor formatting

--- cptac/cancers/bcm/bcmccrcc.py
+++ cptac/cancers/bcm/bcmccrcc.py
@@ -36,69 +36,69 @@ class BcmCcrcc(Source):
 
         self.data_files = {
             "1.0": {
-                "mapping" : "gencode.v34.basic.annotation-mapping.txt",
                 "circular_RNA" : "CCRCC-circRNA_rsem_tumor_normal_UQ_log2(x+1)_BCM.txt",
+                "mapping" : "gencode.v34.basic.annotation-mapping.txt",
                 "transcriptomics" : "CCRCC-gene_rsem_removed_circRNA_tumor_normal_UQ_log2(x+1)_BCM.txt"
             }
         }
-        
+
         self.load_functions = {
             'circular_RNA' : self.load_circular_RNA,
             'transcriptomics' : self.load_transcriptomics,
         }
-        
+
         if version == "latest":
             version = sorted(self.valid_versions)[-1]
 
         # Call the parent class __init__ function
         super().__init__(cancer_type="ccrcc", source='bcm', version=version, valid_versions=self.valid_versions, data_files=self.data_files, no_internet=no_internet)
-        
-        
+
+
     def load_circular_RNA(self):
         df_type = 'circular_RNA'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.perform_initial_checks(df_type)
-            
+
             df = pd.read_csv(file_path, sep="\t")
             df = df.rename_axis('INDEX').reset_index()
             df[["circ","chrom","start","end","gene"]] = df.INDEX.str.split('_', expand=True)
             df["circ_chromosome"] = df["circ"] +"_" + df["chrom"]
             df = df.set_index('gene')
             self._data["circular_RNA"] = df
-            
+
             # Add gene names to circular RNA data
             self.load_mapping()
             gene_key = self._helper_tables["gene_key"]
-    
+
             df = gene_key.join(df, how = "inner")
             df = df.reset_index()
             df = df.rename(columns= {"gene_name": "Name","gene":"Database_ID"}) # change names to match cptac package
             df = df.set_index(["Name","circ_chromosome", "start","end","Database_ID"]) #create multi-index
-            df.drop(['INDEX', 'circ', 'chrom'], axis=1, inplace=True) 
+            df.drop(['INDEX', 'circ', 'chrom'], axis=1, inplace=True)
             df = df.sort_index()
             df = df.T
             df.index = df.index.str.replace(r"_T", "", regex=True) # remove Tumor label
             df.index = df.index.str.replace(r"_A", ".N", regex=True)# Normal samples labeled with .N
             df.index.name = "Patient_ID"
             self._data["circular_RNA"] = df
-        
+
     def load_mapping(self):
         df_type = 'mapping'
         # self._helper_tables is a dictionary of helpful dataframes that the user does not need to access
         # dataframes here are used to load the other data types, but don't show up when the user lists available data
         # this way mapping only needs to be loaded once and all other types can use it when they are loaded
-        if df_type not in self._helper_tables:
-            
+        if "gene_key" not in self._helper_tables:
+
             file_path = self.perform_initial_checks(df_type)
-            
+
             df = pd.read_csv(file_path, sep="\t")
             df = df[["gene","gene_name"]] #only need gene (database gene id) and gene_name (common gene name)
             df = df.set_index("gene")
             df = df.drop_duplicates()
-            self._helper_tables["gene_key"] = df 
-            
+            self._helper_tables["gene_key"] = df
+
 
     def load_transcriptomics(self):
         df_type = 'transcriptomics'
@@ -106,10 +106,10 @@ class BcmCcrcc(Source):
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.perform_initial_checks(df_type)
-            
+
             df = pd.read_csv(file_path, sep="\t")
             df.index.name = 'gene'
-            
+
             # Add gene names to transcriptomic data
             self.load_mapping()
             gene_key = self._helper_tables["gene_key"]
@@ -123,11 +123,9 @@ class BcmCcrcc(Source):
             transcript.index = transcript.index.str.replace(r"_A", ".N", regex=True)# Normal samples labeled with .N
             transcript.index.name = "Patient_ID"
             self._data["transcriptomics"] = transcript
-       
 
-        
-       
-    # Ignore this for now lol    
+
+
+
+    # Ignore this for now lol
     # self._data = sort_all_rows_pancan(self._data) # Sort IDs (tumor first then normal)
-
-
