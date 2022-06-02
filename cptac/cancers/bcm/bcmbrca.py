e2d31ef2b512d8753b53eae68e6b18d09bea5f66
Robert Oldroyd <oldroydrobert@gmail.com>
2022-06-01 21:40:35 -0600

    refactor bcm

--- cptac/cancers/bcm/bcmbrca.py
+++ cptac/cancers/bcm/bcmbrca.py
@@ -13,78 +13,88 @@ import pandas as pd
 import numpy as np
 import os
 import warnings
-import datetime
 
 from cptac.cancer import Cancer
 from cptac.tools.dataframe_tools import *
 from cptac.exceptions import FailedReindexWarning, PublicationEmbargoWarning, ReindexMapError
 
 
-class BcmBrca(Cancer):
+class BcmBrca(Source):
 
-    def __init__(self, no_internet, version):
-        """Load all of the bcmbrca dataframes as values in the self._data dict variable, with names as keys, and format them properly.
+    def __init__(self, version="latest", no_internet=False):
+        """Define which bcmbrca dataframes as are available in the self.load_functions dictionary variable, with names as keys.
 
         Parameters:
-        version (str, optional): The version number to load, or the string "latest" to just load the latest building. Default is "latest".
+        version (str, optional): The version number to load, or the string "latest" to just load the latest datafreeze. Default is "latest".
         no_internet (bool, optional): Whether to skip the index update step because it requires an internet connection. This will be skipped automatically if there is no internet at all, but you may want to manually skip it if you have a spotty internet connection. Default is False.
         """
 
         # Set some needed variables, and pass them to the parent Dataset class __init__ function
 
         # This keeps a record of all versions that the code is equipped to handle. That way, if there's a new data release but they didn't update their package, it won't try to parse the new data version it isn't equipped to handle.
-        valid_versions = ["1.0"]
+        self.valid_versions = ["1.0"]
 
-        data_files = {
-            "1.0": [
-                "BRCA-gene_RSEM_tumor_normal_UQ_log2(x+1)_BCM.txt", 
-                "gencode.v34.basic.annotation-mapping.txt"
-            ]
+        self.data_files = {
+            "1.0": {
+                "transcriptomics" : "BRCA-gene_RSEM_tumor_normal_UQ_log2(x+1)_BCM.txt", 
+                "mapping" : "gencode.v34.basic.annotation-mapping.txt"
+            }
         }
+        
+        self.load_functions = {
+            'transcriptomics' : self.load_transcriptomics,
+        }
+        
+        if version == "latest":
+            version = sorted(self.valid_versions)[-1]
 
         # Call the parent class __init__ function
-        super().__init__(cancer_type="bcmbrca", version=version, valid_versions=valid_versions, data_files=data_files, no_internet=no_internet)
+        super().__init__(cancer_type="brca", source='bcm', version=version, valid_versions=self.valid_versions, data_files=self.data_files, no_internet=no_internet)
 
-        # Load the data into dataframes in the self._data dict
-        loading_msg = f"Loading {self.get_cancer_type()} v{self.version()}"
-        for file_path in self._data_files_paths: # Loops through files variable
-
-            # Print a loading message. We add a dot every time, so the user knows it's not frozen.
-            loading_msg = loading_msg + "."
-            print(loading_msg, end='\r')
-
-            path_elements = file_path.split(os.sep) # Get a list of the levels of the path
-            file_name = path_elements[-1] # The last element will be the name of the file. We'll use this to identify files for parsing in the if/elif statements below
-
-            if file_name == "BRCA-gene_RSEM_tumor_normal_UQ_log2(x+1)_BCM.txt":
-                df = pd.read_csv(file_path, sep="\t")
-                df.index.name = 'gene'
-                self._data["transcriptomics"] = df
-                
-            if file_name == "gencode.v34.basic.annotation-mapping.txt":
-                df = pd.read_csv(file_path, sep="\t")
-                df = df[["gene","gene_name"]] #only need gene (database gene id) and gene_name (common gene name)
-                df = df.set_index("gene")
-                df = df.drop_duplicates()
-                self._helper_tables["gene_key"] = df 
-
-
-        print(' ' * len(loading_msg), end='\r') # Erase the loading message
-        formatting_msg = f"Formatting {self.get_cancer_type()} dataframes..."
-        print(formatting_msg, end='\r')
-     
-        # Add gene names to transcriptomic data         
-        trans_df = self._data["transcriptomics"]
-        gene_key = self._helper_tables["gene_key"]
-        transcript = gene_key.join(trans_df,how = "inner") #keep only gene_ids with gene names
-        transcript = transcript.reset_index()
-        transcript = transcript.rename(columns={"gene_name":"Name","gene":"Database_ID"})
-        transcript = transcript.set_index(["Name", "Database_ID"])
-        transcript = transcript.sort_index() #alphabetize
-        transcript = transcript.T
-        transcript.index.name = "Patient_ID"
-        self._data["transcriptomics"] = transcript
         
-        self._data = sort_all_rows_pancan(self._data)  # Sort IDs (tumor first then normal)
+    def load_mapping(self):
+        df_type = 'mapping'
+        # self._helper_tables is a dictionary of helpful dataframes that the user does not need to access
+        # dataframes here are used to load the other data types, but don't show up when the user lists available data
+        # this way mapping only needs to be loaded once and all other types can use it when they are loaded
+        if df_type not in self._helper_tables:
+            
+            file_path = self.perform_initial_checks(df_type)
+            
+            df = pd.read_csv(file_path, sep="\t")
+            df = df[["gene","gene_name"]] #only need gene (database gene id) and gene_name (common gene name)
+            df = df.set_index("gene")
+            df = df.drop_duplicates()
+            self._helper_tables["gene_key"] = df 
+            
+        
+    def load_transcriptomics(self):
+        """Load and parse all files for bcm brca transcriptomics data
+           Populates self._data with transcriptomics data in a Pandas dataframe
+        """
+        df_type = 'transcriptomics'
+        if df_type not in self._data:
+            # get file path to the correct data (defined in source.py, the parent class)
+            file_path = self.perform_initial_checks(df_type)
 
-        print(" " * len(formatting_msg), end='\r') # Erase the formatting message
+            # process the file and add it to self._data
+            df = pd.read_csv(file_path, sep='\t')
+            df.index.name = 'gene'
+            # in order to finish parsing the file we need mapping information (the gene_key)
+            # make sure self._helper_tables has gene_key parsed and loaded
+            self.load_mapping()
+            # get gene key to use for transcriptomics
+            gene_key = self._helper_tables["gene_key"]
+            transcript = gene_key.join(df,how = "inner") #keep only gene_ids with gene names
+            transcript = transcript.reset_index()
+            transcript = transcript.rename(columns={"gene_name":"Name","gene":"Database_ID"})
+            transcript = transcript.set_index(["Name", "Database_ID"])
+            transcript = transcript.sort_index() #alphabetize
+            transcript = transcript.T
+            transcript.index.name = "Patient_ID"
+            self._data["transcriptomics"] = transcript
+            
+            
+            
+    # Last hitch in the road
+    self._data = sort_all_rows_pancan(self._data)  # Sort IDs (tumor first then normal)
