c45538841b036591cf031b22031b9c763ff5ca30
Robert Oldroyd <oldroydrobert@gmail.com>
2022-08-02 11:40:36 -0600

    Mark normal samples in all datatypes

--- cptac/cancers/awgconf/awgconfucec.py
+++ cptac/cancers/awgconf/awgconfucec.py
@@ -24,7 +24,7 @@ class AwgConfUcec(Source):
 
     def __init__(self, version="latest", no_internet=False):
         """Define which dataframes as are available in the self.load_functions dictionary variable, with names as keys.
-        
+
         Parameters:
         version (str, optional): The version number to load, or the string "latest" to just load the latest datafreeze. Default is "latest".
         no_internet (bool, optional): Whether to skip the index update step because it requires an internet connection. This will be skipped automatically if there is no internet at all, but you may want to manually skip it if you have a spotty internet connection. Default is False.
@@ -173,28 +173,28 @@ class AwgConfUcec(Source):
 
     def load_acetylproteomics_gene(self):
         df_type = 'acetylproteomics_gene'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t', index_col=0)
             df = df.transpose()
             df = df.sort_index()
             df.index.name = "Patient_ID"
             df.columns.name = "Name"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-    
+
+
     def load_acetylproteomics(self):
         df_type = 'acetylproteomics'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t', index_col=0)
             df = df.reset_index()
             df[['Name','Database_ID','Site']] = df.idx.str.split("@", expand=True)
@@ -204,88 +204,84 @@ class AwgConfUcec(Source):
             df = df.transpose()
             df = df.sort_index()
             df.index.name = "Patient_ID"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-            
+
+
     def load_clinical(self):
         df_type = 'clinical'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_excel(file_path)
-            df.insert(6, "Proteomics_Tumor_Normal", df["Group"])
-            df.loc[df['Group'] == 'Enriched_Normal', 'Idx'] = df['Idx'] + '.N'
-            df.loc[df['Group'] == 'Adjacent_normal', 'Idx'] = df['Idx'].str[:-2] + '.N'
             df = df.set_index("Idx")
-            df.loc[df['Group'] != 'Tumor', 'Group'] = 'Normal'
-            df = df.rename({'Group': 'Sample_Tumor_Normal'}, axis=1)
+            df = df.rename({'Group': 'Proteomics_Tumor_Normal'}, axis=1)
             df.index.name = "Patient_ID"
             df.columns.name = "Name"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-            
+
+
     def load_methylation(self):
         df_type = 'methylation'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t', index_col=0, na_values='   NA')
             df = df.transpose()
             df.index.name = "Patient_ID"
             df.columns.name = "Name"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
+
     def load_miRNA(self):
         df_type = 'miRNA'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t', index_col=0)
             df = df.transpose()
             df = df.sort_index()
             df.index.name = "Patient_ID"
             df.columns.name = "Name"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-            
+
+
     def load_phosphoproteomics_gene(self):
         df_type = 'phosphoproteomics_gene'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t', index_col=0)
             df = df.transpose()
             df = df.sort_index()
             df.index.name = "Patient_ID"
             df.columns.name = "Name"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-            
+
+
     def load_phosphoproteomics(self):
         df_type = 'phosphoproteomics'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t', index_col=0)
             df = df.reset_index()
             df[['Name','Database_ID','Site']] = df.idx.str.split("@", expand=True)
@@ -295,77 +291,77 @@ class AwgConfUcec(Source):
             df = df.transpose()
             df = df.sort_index()
             df.index.name = "Patient_ID"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
+
     def load_proteomics(self):
         df_type = 'proteomics'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t', index_col=0)
             df = df.transpose()
             df = df.sort_index()
             df.index.name = "Patient_ID"
             df.columns.name = "Name"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-            
+
+
     def load_circular_RNA(self):
         df_type = 'circular_RNA'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t', index_col=0)
             df = df.transpose()
             df.index.name = "Patient_ID"
             df.columns.name = "Name"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-            
+
+
     def load_gene_fusion(self):
         df_type = 'gene_fusion'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t', index_col=0)
             df = df.reset_index()
             df = df.set_index("Sample")
             df.index.name = "Patient_ID"
             df.columns.name = "Name"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-    
+
+
     def load_transcriptomics(self):
         df_type = 'transcriptomics'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t', index_col=0)
             df = df.transpose()
             df = df.sort_index()
             df.index.name = "Patient_ID"
             df.columns.name = "Name"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-            
+
+
     def load_targeted_proteomics(self):
         df_type = 'targeted_proteomics'
 
@@ -375,7 +371,7 @@ class AwgConfUcec(Source):
             for file_path in file_path_list:
                 path_elements = file_path.split(os.sep) # Get a list of the levels of the path
                 file_name = path_elements[-1] # The last element will be the name of the file. We'll use this to identify files for parsing in the if/elif statements below
-                
+
                 # Load and parse information from PRISM and Direct files
                 if "UCEC_confirmatory_SRM_Direct_tumor" in file_name:
                     df_direct = pd.read_csv(file_path, sep='\t')
@@ -385,7 +381,7 @@ class AwgConfUcec(Source):
                     df_direct = df_direct.transpose()
                     df_direct = df_direct.sort_index()
                     df_direct.index.name = "Patient_ID"
-                    
+
                 elif "UCEC_confirmatory_SRM_PRISM_tumor" in file_name:
                     df_prism = pd.read_csv(file_path, sep='\t')
                     df_prism[['Name','Peptide']] = df_prism['idx'].str.rsplit("-", 1, expand=True)
@@ -399,18 +395,18 @@ class AwgConfUcec(Source):
             df_combined = pd.concat([df_direct, df_prism])
             df_combined.index.name = "Patient_ID"
             df_combined.columns.name = "Name"
-            
+
             # save df in self._data
             self.save_df(df_type, df_combined)
-            
-            
+
+
     def load_targeted_phosphoproteomics(self):
         df_type = 'targeted_phosphoproteomics'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t')
             df.at[0,'idx'] = "FPSS[+80]PLRIPGGNIY[+80]ISPLK"
             df['Name'] = "RB1"
@@ -420,34 +416,34 @@ class AwgConfUcec(Source):
             df = df.sort_index()
             df.columns.name = "Name"
             df.index.name = "Patient_ID"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-    
+
+
     def load_somatic_mutation_binary(self):
         df_type = 'somatic_mutation_binary'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t', index_col=0)
             df = df.transpose()
             df.index.name = "Patient_ID"
             df.columns.name = "Name"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-            
+
+
     def load_somatic_mutation(self):
         df_type = 'somatic_mutation'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t', dtype={88:object})
             df = df[['Tumor_Sample_Barcode','Hugo_Symbol','Variant_Classification','HGVSp_Short']]
             df['Tumor_Sample_Barcode'] = df['Tumor_Sample_Barcode'].apply(lambda s: s[:-2])
@@ -459,18 +455,18 @@ class AwgConfUcec(Source):
             df = df.sort_values(by=["Patient_ID", "Gene"])
             df.columns.name = "Name"
             df = df.set_index("Patient_ID")
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-            
+
+
     def load_CNV_gistic(self):
         df_type = 'CNV_gistic'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t')
             df[['Name','Chromosome']] = df.idx.str.split("|", expand=True)
             df = df.set_index(["Name"])
@@ -478,18 +474,18 @@ class AwgConfUcec(Source):
             df = df.transpose()
             df = df.sort_index()
             df.index.name = "Patient_ID"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
-            
-            
+
+
     def load_CNV_log2ratio(self):
         df_type = 'CNV_log2ratio'
-        
+
         if df_type not in self._data:
             # perform initial checks and get file path (defined in source.py, the parent class)
             file_path = self.locate_files(df_type)
-            
+
             df = pd.read_csv(file_path, sep='\t')
             df[['Name','Chromosome']] = df.idx.str.split("|", expand=True)
             df = df.set_index(["Name"])
@@ -497,13 +493,18 @@ class AwgConfUcec(Source):
             df = df.transpose()
             df = df.sort_index()
             df.index.name = "Patient_ID"
-            
+
             # save df in self._data
             self.save_df(df_type, df)
 
 
     # Override the save_df function from source.py so we can give an access warning the first time the data is used
     def save_df(self, datatype, df):
+
+        # Mark normal samples in each datatype
+        df.index = df.index.where(~df.index.str.endswith('-A'), df.index.str[:-2] + ".N")
+        df.index = df.index.where(~df.index.str.startswith('N'), df.index.str[:] + ".N")
+
         if self._data == {}:
             # Print password access only warning
             warnings.warn("The UcecConf data is currently strictly reserved for CPTAC investigators. Otherwise, you are not authorized to access these data. Additionally, even after these data become publicly available, they will be subject to a publication embargo (see https://proteomics.cancer.gov/data-portal/about/data-use-agreement or enter cptac.embargo() to open the webpage for more details).", PublicationEmbargoWarning, stacklevel=2)
