d829ddbfc757951377da1c216fac769e1de1a67e
Samuel Squires <sdsquires@gmail.com>
2023-03-23 15:46:41 -0600

    Removed testing artifacts

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(name='cptac',
         'License :: OSI Approved :: Apache Software License',
 	],
 	keywords='bioinformatics cancer proteomics genomics open science open data',
-	python_requires='>=3.6.*',
+	python_requires='>=3.6',
 	zip_safe=False,
 	include_package_data=True,
 	project_urls={
