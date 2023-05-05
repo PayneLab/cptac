5936e0ce8909ed7a019af00e3be63a9c373b9536
Blake McGee <blakemcgee@Blakes-Air-2.app.byu.edu>
2023-05-05 16:11:24 -0600

    More changes

--- cptac/tools/download_tools/box_download.py
+++ cptac/tools/download_tools/box_download.py
@@ -90,7 +90,7 @@ def get_file_names(cancer, source, datatypes, index_path):
     with open('all_index.txt', 'r') as input:
         for line in input:
             indexList = line.split('\t')
-            file_identifiers = indexList.split('_')
+            file_identifiers = indexList[0].split('_')
             if file_identifiers[2] in datatypes and file_identifiers[0] == source and file_identifiers[1] == cancer:
                 name = indexList[1]
             file_names.append(name)
