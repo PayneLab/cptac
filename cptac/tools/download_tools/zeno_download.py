d663556200a52a4f6b818ab21bae0b20ded287ab
Sabrina Smith <sabmel11@gmail.com>
2023-05-12 15:29:37 -0600

    Removed bloat in zeno_download

--- cptac/tools/download_tools/zeno_download.py
+++ cptac/tools/download_tools/zeno_download.py
@@ -1,14 +1,5 @@
-import getpass
-import glob
 import os
-import packaging.version
 import requests
-import warnings
-import zenodopy
-import wget
-import requests
-import urllib.parse
-import pandas as pd
 from tqdm import tqdm
 import concurrent.futures
 from concurrent.futures import ThreadPoolExecutor
@@ -120,65 +111,6 @@ def get_data(url: str, subfolder: str = '', num_threads: int = 4) -> str:
         raise
 
 
-
-# def download_index_file_if_needed():
-#     """
-#     Downloads the index file if it does not already exist in the data directory
-
-#     :return: The path to the index file
-#     """
-#     index_path = os.path.join(DATA_DIR, INDEX_FILE_NAME)
-#     if not os.path.exists(index_path):
-#         index_url = get_index_file_url()
-#         if not index_url:
-#             raise FileNotFoundError(f"Index file '{INDEX_FILE_NAME}' not found in Zenodo record (DOI: {STATIC_DOI})")
-#         wget.download(index_url, index_path)
-
-#     return index_path
-
-# def get_index_file_url():
-#     """
-#     Gets the URL of the index file from the Zenodo record.
-
-#     :return: The URL of the index file.
-#     :raise FileNotFoundError: If the index file is not found in the Zenodo record
-#     """
-#     zenodo = zenodopy.Client()
-#     record = zenodo.get_urls_from_doi(STATIC_DOI)
-
-#     if not record:
-#         return None
-
-#     for url in record:
-#         if url.endswith(INDEX_FILE_NAME):
-#             return url
-
-#     raise FileNotFoundError(f"Index file '{INDEX_FILE_NAME}' not found in Zenodo record (DOI: {STATIC_DOI})")
-
-#     return None
-
-# def get_file_names(cancer, source, datatypes, index_path):
-#     """
-#     Gets the URLs of the files that match the given cancer, source, and datatypes
-
-#     :param cancer: The cancer type (e.g. 'brca)
-#     :param source: The data source (e.g. 'harmonized)
-#     :param datatype: The datatype of the files to download (e.g. 'clinical')
-#     :param index_path: The path to the index file.
-#     :return: A list of file URLs that match the given parameters
-#     """
-#     file_names = []
-
-#     with open(index_path, 'r') as f:
-#         for line in f:
-#             tokens = line.strip().split('\t')
-#             file_identifiers = tokens[0].split('_')
-
-#             if file_identifiers[0] == source and file_identifiers[1] == cancer and file_identifiers[2] in datatypes:
-#                 file_name = tokens[1]
-#                 file_names.append(file_name)
-#     return file_names
-
 def download_text(url):
     """Download text from a direct download url for a text file.
 
@@ -197,29 +129,3 @@ def download_text(url):
 
     text = response.text.strip()
     return text
-
-# def parse_tsv_dict(path):
-#     """Read in a dictionary from the given two column tsv file.
-
-#     Parameters:
-#     path (str): The path to the two column tsv file.
-
-#     Returns:
-#     dict: The tsv file read into a dictionary.
-#     """
-#     if not os.path.isfile(path):
-#         raise MissingFileError(f"Missing file {path}. Please update the cptac package to restore.")
-
-#     with open(path, 'r') as data_file:
-#         lines = data_file.readlines()
-
-#     data_dict = {}
-#     for line in lines:
-#         if len(line.strip()) == 0:
-#             continue
-#         line_list = line.strip().split("\t")
-#         key = line_list[0]
-#         value = line_list[1]
-#         data_dict[key] = value
-
-#     return data_dict
